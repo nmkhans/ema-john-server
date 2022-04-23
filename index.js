0a36eb67b697a2ac354a493a82a59121be73f84a
Moin Khan <nurmoin05@gmail.com>
2022-04-24 01:19:33 +0600

    Database intrigation completed

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+
 //? Middle Were
 app.use(cors());
 app.use(express.json());
 
+
 //? Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qruro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-//? Servers
 
+//? Server Stablish
 const server = async () => {
     try {
+        await client.connect();
+        const database = client.db('ema-john').collection('product');
 
     }
 
     finally {
-
+        //// client.close();
     }
 }
 
-server().catch(console.log);
+server().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('server is running');
